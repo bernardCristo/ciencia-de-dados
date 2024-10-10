86ba7e8e2c5f2e67a8530629369843c4068f712e
bernard <102028892+bernardCristo@users.noreply.github.com>
2024-10-10 09:36:03 -0300

    Update commons.js

--- commons.js
+++ commons.js
@@ -1,1 +1,39 @@
+const getcss (variavel) => {
 
+
+
+const bodystyles getComputedStyle(document.body)
+
+
+
+return bodystyles.getPropertyValue(variavel)
+
+
+
+}
+
+
+
+
+
+const tickConfig = (
+
+
+
+family: getCSS("--font'),
+
+size: 16,
+
+
+
+color: getCSS('--primary-color')
+
+
+
+}
+
+
+
+
+
+export (getCSS, tickConfig)
